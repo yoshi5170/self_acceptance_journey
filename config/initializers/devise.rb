a554e48ece192ec554b8bbb535dc3087831af4b3
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-09-20 17:09:06 +0900

    add: Deviseの設定

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,6 +272,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :twitter, Rails.application.credentials.x[:API_KEY], Rails.application.credentials.x[:API_SECRET_KEY]
+  OmniAuth.config.logger = Rails.logger if Rails.env.development? # debug用
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
