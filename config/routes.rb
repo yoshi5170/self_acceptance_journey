fd23d8be4bb6aedb4e6c45b0c36aaae7c9d40cc7
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-09-20 17:10:00 +0900

    OmniAuthのコールバックルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
