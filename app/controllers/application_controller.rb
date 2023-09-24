78a22f95bdc3e85aa03731743a89d67e2daeeb03
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-09-24 22:33:20 +0900

    application_controller.rbにメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :user_signed_in?
+
+    private
+
+    def authenticate_user!
+      redirect_to root_path unless session[:user_id]
+    end
+
+    def current_user
+      return unless session[:user_id]
+      @current_user ||= User.find_by(uid: session[:user_id])
+    end
+
+    def user_signed_in?
+      !!session[:user_id]
+    end
 end
