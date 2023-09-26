d6c6fc15ddfd9c400171d014b9ed3ca0b439a769
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-09-26 22:30:39 +0900

    application controllerのメソッドを削除、require_loginを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,11 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user, :user_signed_in?
+  before_action :require_login
 
-    private
+  private
 
-    def authenticate_user!
-      redirect_to root_path unless session[:user_id]
+  def require_login
+    unless logged_in?
+      redirect_to root_url
     end
-
-    def current_user
-      return unless session[:user_id]
-      @current_user ||= User.find_by(uid: session[:user_id])
-    end
-
-    def user_signed_in?
-      !!session[:user_id]
-    end
+  end
 end
