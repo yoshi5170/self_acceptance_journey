48667af080863c72c032a3949e29317c90674d13
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-09-26 22:31:34 +0900

    session[:user_id] に @user.idが保存されるように変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: :create
   def create
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    session[:user_id] = @user.uid
+    session[:user_id] = @user.id
     redirect_to request.env["omniauth.origin"] || root_url
   end
 
