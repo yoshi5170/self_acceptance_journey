83464cb71924df33baddc8620dd4a53149995c7a
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-09-29 14:57:17 +0900

    add: CreateUnlockableFlowersマイルレーションファイルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_131249) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_29_054712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "unlockable_flowers", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "threshold", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
