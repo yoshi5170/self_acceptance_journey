57a73daf8d2105a44c963a99c2b7a1b3ef813e71
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-09-29 15:20:14 +0900

    add: Create planted flowersマイグレーションファイルを作成

---
+++ db/migrate/20230929061052_create_planted_flowers.rb
@@ -0,0 +1,11 @@
+class CreatePlantedFlowers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :planted_flowers do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :unlockable_flower, null: false, foreign_key: true
+      t.datetime :added_at, null: false
+
+      t.timestamps
+    end
+  end
+end
