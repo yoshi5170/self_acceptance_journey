9abefb104eae9398a8069fa720c3e701b1b9d3cc
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-04 00:07:46 +0900

    add: 花のスタンプをunlockable_flowerに保存する処理を追加

--- app/controllers/admin/unlockable_flowers_controller.rb
+++ app/controllers/admin/unlockable_flowers_controller.rb
@@ -1,4 +1,20 @@
-class Admin::UnlockableFlowersController < ApplicationController
+class Admin::UnlockableFlowersController < Admin::BaseController
   def new
+    @unlockable_flower = UnlockableFlower.new
+  end
+
+  def create
+    @unlockable_flower = UnlockableFlower.new(unlockable_flower_params)
+    if @unlockable_flower.save
+      redirect_to admin_unlockable_flowers_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def unlockable_flower_params
+    params.require(:unlockable_flower).permit(:name, :threshold, :flower_image)
   end
 end
