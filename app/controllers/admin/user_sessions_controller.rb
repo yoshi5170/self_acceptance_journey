0462441d5237a8e287668a1ee4eb42c3dd6a0a03
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-04 00:08:38 +0900

    add: 管理者画面ログインの処理を追加

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,4 +1,24 @@
-class Admin::UserSessionsController < ApplicationController
+class Admin::UserSessionsController < Admin::BaseController
+  skip_before_action :not_authenticated, only: %i[new create]
+  layout 'admin_login'
   def new
+  end
+
+  def create
+    email = Rails.application.credentials.admin[:email]
+    password = Rails.application.credentials.admin[:password]
+    id = Rails.application.credentials.admin[:id]
+
+    if params[:email] == email && params[:password] == password
+      session[:admin_logged_in] = id
+      redirect_to admin_root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:admin_logged_in)
+    redirect_to admin_login_path#, notice: t('defaults.message.logged_out')
   end
 end
