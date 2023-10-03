1532bdd69c64d639af6f29cb3d6ecf42ad0531ec
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-04 00:03:39 +0900

    管理画面で使用するメソッドを追加

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,2 +1,14 @@
 class Admin::BaseController < ApplicationController
+  layout 'admin/layouts/application'
+  helper_method :admin_logged_in?
+  before_action :not_authenticated
+  def admin_logged_in?
+    session[:admin_logged_in].present?
+  end
+
+  def not_authenticated
+    unless admin_logged_in?
+      redirect_to admin_login_path, warning: t('defaults.message.require_login')
+    end
+  end
 end
