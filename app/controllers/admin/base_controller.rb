07e9c0664ae0ab47669a937ddb5d6c6d563362a3
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-06 08:51:50 +0900

    Admin::BaseController内のメソッドを編集

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,9 +1,11 @@
 class Admin::BaseController < ApplicationController
   layout 'admin/layouts/application'
+  skip_before_action :require_login
   helper_method :admin_logged_in?
   before_action :not_authenticated
+
   def admin_logged_in?
-    session[:admin_logged_in].present?
+    session[:admin_logged_in].present? && session[:admin_logged_in] == Rails.application.credentials.admin[:id]
   end
 
   def not_authenticated
