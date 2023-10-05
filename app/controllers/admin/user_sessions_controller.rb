19b0a77a7741a3bda61172df3c611efed1b88f84
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-06 08:22:47 +0900

    add: Admin::UserSessionsControllerにフラッシュメッセージを追加

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,8 +1,8 @@
 class Admin::UserSessionsController < Admin::BaseController
   skip_before_action :not_authenticated, only: %i[new create]
   layout 'admin_login'
-  def new
-  end
+
+  def new; end
 
   def create
     email = Rails.application.credentials.admin[:email]
@@ -11,14 +11,15 @@ class Admin::UserSessionsController < Admin::BaseController
 
     if params[:email] == email && params[:password] == password
       session[:admin_logged_in] = id
-      redirect_to admin_root_path
+      redirect_to admin_root_path, success: t('.success')
     else
-      render :new
+      flash.now[:danger] = t('.fail')
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     session.delete(:admin_logged_in)
-    redirect_to admin_login_path#, notice: t('defaults.message.logged_out')
+    redirect_to admin_login_path, success: t('.success'), status: :see_other
   end
 end
