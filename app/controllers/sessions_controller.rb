ca167ed52c85cb6046d40ab9b5555705b780dce8
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-06 08:20:54 +0900

    SessionsControllerにフラッシュメッセージを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,16 @@ class SessionsController < ApplicationController
   def create
     @user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = @user.id
-    redirect_to request.env["omniauth.origin"] || root_url
+    redirect_to request.env["omniauth.origin"] || root_url, success: t('.success')
   end
 
   def destroy
     reset_session
-    redirect_to root_url, status: :see_other
+    redirect_to root_url, success: t('.success'), status: :see_other
   end
 
   def failure
-    redirect_to root_url
+    redirect_to root_url, danger: t('.fail')
   end
 
 end
