919f21fd28f3230c9e49e5bd7d0e7889c6252d27
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-06 19:15:54 +0900

    管理画面で動的にタイトルが表示されるようにpage_titleメソッドを編集

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
-  def page_title(page_title = '')
-    base_title = 'AccepttMe'
+  def page_title(page_title = '', admin = false)
+    base_title =  if admin
+                    'AccepttMe(管理画面)'
+                  else
+                    'AccepttMe'
+                  end
 
     page_title.empty? ? base_title : page_title + '|' + base_title
   end
