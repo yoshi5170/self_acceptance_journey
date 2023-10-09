8e38d52bc1280db915bd20f89903398153bc4383
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-09 22:14:47 +0900

    add: flower.jsファイルを追加

---
+++ app/javascript/ flower.js
@@ -0,0 +1,27 @@
+console.log("JavaScript file loaded!");
+document.addEventListener('DOMContentLoaded', () => {
+  const convertButton = document.getElementById('convertButton');
+  if (!convertButton) return;
+  console.log("Convert Button:", convertButton);
+  const form = convertButton.closest('form');
+  console.log("Closest Form:", form);
+  const animationArea = document.getElementById('animationArea');
+  const inputArea = document.querySelector('.input-area');
+  console.log("Input Area:", inputArea);
+
+  convertButton.addEventListener('click', (event) => {
+    console.log("Button clicked!");
+    // デフォルトのフォーム送信をキャンセル
+    event.preventDefault();
+
+    inputArea.classList.add('hidden');
+
+    // GIFアニメーションを表示
+    animationArea.classList.remove('hidden');
+
+    // 例として5秒後にフォームを送信
+    setTimeout(() => {
+      form.submit();
+    }, 5000); // 5000ミリ秒 = 5秒
+  });
+});
