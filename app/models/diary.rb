bd6cc5fc33e7e168364aeef457809a08521c8b96
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-25 20:35:13 +0900

    uniquenessバリデーションをDiaryモデルに追加

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,11 @@
 class Diary < ApplicationRecord
   belongs_to :user
   has_many :diary_entries, dependent: :destroy
+  validates :date,
+    uniqueness: { scope: :user_id,
+      message: ->(object, data) do
+        " #{data[:value].to_date}の日記はすでに作成済みです"
+      end
+    }
+
 end
