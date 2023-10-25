7db16a78cecd4d36b3cf9a8d75083856e9f09993
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-25 14:00:08 +0900

    add: MonthlyThemesテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_24_142927) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_25_045709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_142927) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["diary_id"], name: "index_diary_entries_on_diary_id"
+  end
+
+  create_table "monthly_themes", force: :cascade do |t|
+    t.integer "month", null: false
+    t.string "title", null: false
+    t.text "message", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "planted_flowers", force: :cascade do |t|
