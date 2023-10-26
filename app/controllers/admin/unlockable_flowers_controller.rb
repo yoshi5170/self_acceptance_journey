e9e08866c2e12f8210c15fc683b87ff493983b1f
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-26 16:28:36 +0900

    花の編集、更新、削除の処理を追加

--- app/controllers/admin/unlockable_flowers_controller.rb
+++ app/controllers/admin/unlockable_flowers_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UnlockableFlowersController < Admin::BaseController
+  before_action :set_unlockable_flower, only: %i[edit update destroy]
+
   def index
     @unlockable_flowers= UnlockableFlower.all
   end
@@ -6,6 +8,7 @@ class Admin::UnlockableFlowersController < Admin::BaseController
   def new
     @unlockable_flower = UnlockableFlower.new
   end
+
 
   def create
     @unlockable_flower = UnlockableFlower.new(unlockable_flower_params)
@@ -16,7 +19,27 @@ class Admin::UnlockableFlowersController < Admin::BaseController
     end
   end
 
+  def edit; end
+
+  def update
+    if @unlockable_flower.update(unlockable_flower_params)
+      redirect_to admin_unlockable_flowers_path, success: '花を更新しました'
+    else
+      flash.now[:danger] = '花の更新に失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @unlockable_flower.destroy!
+    redirect_to admin_unlockable_flowers_path, success: '質問を削除しました'
+  end
+
   private
+
+  def set_unlockable_flower
+    @unlockable_flower = UnlockableFlower.find(params[:id])
+  end
 
   def unlockable_flower_params
     params.require(:unlockable_flower).permit(:name, :threshold, :flower_image)
