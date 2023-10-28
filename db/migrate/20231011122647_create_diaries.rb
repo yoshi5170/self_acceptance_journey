43dbdc9aa4f0d98a74baf2932d11e8cb38d36936
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-28 22:32:31 +0900

    マイグレーションファイルの修正

--- db/migrate/20231011122647_create_diaries.rb
+++ db/migrate/20231011122647_create_diaries.rb
@@ -1,10 +1,12 @@
 class CreateDiaries < ActiveRecord::Migration[7.0]
   def change
     create_table :diaries do |t|
-      t.datetime :date, null: false
+      t.date :date, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :diaries, [:user_id, :date], unique: true
   end
 end
