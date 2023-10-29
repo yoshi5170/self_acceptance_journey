c96b12e457b17c5b75eefd66dfe9c725997b0d43
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-29 19:39:07 +0900

    sorceryの代わりにdeviseを使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,9 @@
 class User < ApplicationRecord
-  authenticates_with_sorcery!
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+  enum role: {general: 0, guest: 1, admin: 2}
   has_many :planted_flowers, dependent: :destroy
   has_many :diaries, dependent: :destroy
-  validates :name, presence: true
-  validates :nickname, presence: true
-  validates :uid, presence: true, uniqueness: true
-
-  def self.from_omniauth(auth)
-    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.nickname = auth.info.nickname
-      user.name = auth.info.name
-    end
-  end
 end
