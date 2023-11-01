4a8553f3831e5c32f5c1d44af870b2516c391eb4
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-11-01 19:05:29 +0900

    SelfEsteemTrainingsController のリファクダリング

--- app/controllers/self_esteem_trainings_controller.rb
+++ app/controllers/self_esteem_trainings_controller.rb
@@ -21,15 +21,13 @@ class SelfEsteemTrainingsController < ApplicationController
     )
     Rails.logger.info "Received response from OpenAI: #{response.inspect}"
     @chat = response.dig("choices", 0, "message", "content") if response.present?
-    #@chat = "私のプレゼンテーションのスキルはまだ改善の余地がありますが、他の人と比べて自分を貶める必要はないと気づきました。失敗から学び、成長することができるチャンスです。私は自分自身を大切にし、自己価値を持ち続けることを選びます。
-              #私のプレゼンテーションのスキルはまだ改善の余地がありますが、他の人と比べて自分を貶める必要はないと気づきました。失敗から学び、成長することができるチャンスです。私は自分自身を大切にし、自己価値を持ち続けることを選びます。"
+
     # レスポンスが存在する場合、新しいトレーニングセッションを作成
-    # if @chat.present?
-    #   @training = current_user.self_negation_trainings.create(trained_at: Time.current)
-    #   add_flower_to_garden
-		# else
-    #   render :new
-    # end
+    if @chat.present?
+      current_user.add_training_and_flower
+		else
+      render :new
+    end
 
   end
 
@@ -42,28 +40,5 @@ class SelfEsteemTrainingsController < ApplicationController
 
   def text_params
     @query = params[:text]
-  end
-
-	def add_flower_to_garden
-		#トレーニングをした合計回数を取得
-    total_trainings = current_user.self_negation_trainings.count + positive_conversion_trainings.count
-
-		return if total_trainings == 0 # トレーニング回数が0の場合は何もしない
-
-    # unlockable_flowersをthresholdの昇順で取得
-		#例）[花A: threshold 10,　花B: threshold 35, 花C: threshold 65, 花D: threshold 100, 花E: threshold 145, 花F: threshold 195]
-    flowers = UnlockableFlower.order(threshold: :asc)
-
-		#total_trainings（トータルのトレーニング回数）がflower.threshold（各花が解禁されるためのトレーニング完了回数の閾値）以下であるかどうかを判断
-		flowers.each do |flower|
-	    if total_trainings <= flower.threshold
-	      flower_to_add = flower
-	      break
-	    end
-	  end
-
-    if flower_to_add
-      current_user.planted_flowers.create(unlockable_flower: flower_to_add, added_at: Time.current)
-    end
   end
 end
--- app/models/unlockable_flower.rb
+++ app/models/unlockable_flower.rb
@@ -1,4 +1,14 @@
 class UnlockableFlower < ApplicationRecord
   has_many :planted_flowers, dependent: :restrict_with_error
   has_one_attached :flower_image
+
+  def self.find_flower(total_trainings)
+    return nil if total_trainings == 0
+    # unlockable_flowersをthresholdの昇順で取得
+		#例）[花A: threshold 10,　花B: threshold 35, 花C: threshold 65, 花D: threshold 100, 花E: threshold 145, 花F: threshold 195]
+    flowers = order(threshold: :asc)
+    #total_trainings（トータルのトレーニング回数）がflower.threshold（各花が解禁されるためのトレーニング完了回数の閾値）以下であるかどうかを判断
+    flowers.find { |flower| total_trainings <= flower.threshold }
+  end
+
 end
