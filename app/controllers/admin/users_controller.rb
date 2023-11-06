a5ed5fc9aeb4822e344fe095e9a89cbf400eb725
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-11-06 20:03:38 +0900

    admin usersコントローラを作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,34 @@
+class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: %i[show edit update destroy]
+  def index
+    @users = User.order(created_at: :desc)
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), success: t('defaults.message.updated', item: User.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_updated', item: User.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy!
+    redirect_to admin_users_path, status: :see_other, success: t('defaults.message.delete', item: User.model_name.human)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :role)
+  end
+end
