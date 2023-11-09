3f503c7b8ae5c7f38481130275c2ee34abc3bcb1
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-11-09 16:01:35 +0900

    FactoryBotの省略とmoduleが読み込まれるように設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  # FactoryBotの省略
+  config.include FactoryBot::Syntax::Methods
+  # module読み込み
+  config.include LoginModule
 end
