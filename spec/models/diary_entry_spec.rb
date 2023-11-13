63423e9b6cd1aa30d457772bfa5a4be816e17b9d
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-11-14 00:02:14 +0900

    DiaryEntryモデルのテストを追加

--- spec/models/diary_entry_spec.rb
+++ spec/models/diary_entry_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe DiaryEntry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:diary) { create(:diary) }
+  describe 'バリデーション' do
+    it 'contentがある場合、有効' do
+      diary_entry = build(:diary_entry)
+      expect(diary_entry).to be_valid
+    end
+    it 'contentがない場合、無効' do
+      diary_entry = build(:diary_entry, content: '')
+      expect(diary_entry).to be_invalid
+    end
+  end
+  describe 'アソシエーション' do
+    it 'diary_enyriesがdiaryと関連付いている' do
+      diary_entry = create(:diary_entry, diary:)
+      expect(diary.diary_entries).to include diary_entry
+      expect(diary_entry.diary_id).to eq(diary.id)
+    end
+  end
 end
