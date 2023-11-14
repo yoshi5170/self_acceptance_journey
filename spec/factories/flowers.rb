21c72b94fad852540a8be54da656598f78d36b16
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-11-15 00:36:17 +0900

    add: flowerのfactorybotを作成

--- spec/factories/flowers.rb
+++ spec/factories/flowers.rb
@@ -1,6 +1,22 @@
 FactoryBot.define do
   factory :flower do
     sequence(:name) { |n| "flower_#{n}" }
-    threshold { 10 }
+
+    trait :threshold_five do
+      threshold { 5 }
+    end
+
+    trait :threshold_ten do
+      threshold { 10 }
+    end
+
+    after(:build) do |flower|
+      filename = flower.threshold == 5 ? 'flower01.png' : 'flower02.png'
+      flower.flower_image.attach(
+        io: File.open("#{Rails.root}/spec/fixtures/image/#{filename}"),
+        filename:,
+        content_type: 'image/png'
+      )
+    end
   end
 end
