e1faf315bb033ad6c02eb63506049027ecf5dcca
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-11-22 16:33:04 +0900

    encouragement_messageとのアソシエーションを追加

--- app/models/encouragement_request.rb
+++ app/models/encouragement_request.rb
@@ -1,6 +1,7 @@
 class EncouragementRequest < ApplicationRecord
   has_one_attached :request_image
   belongs_to :user
+  has_many :encouragement_messages
   validates :text, presence: true, length: { maximum: 150 }
   validates :background_id, presence: true, numericality: { only_integer: true }
 end
