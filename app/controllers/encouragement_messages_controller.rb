083aed8bfc0ae420b1b96a836f233381457ea410
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-11-22 16:31:02 +0900

    add:EncouragementMessagesControllerを追加

---
+++ app/controllers/encouragement_messages_controller.rb
@@ -0,0 +1,60 @@
+class EncouragementMessagesController < ApplicationController
+  before_action :set_encouragement_message, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!
+
+  def index
+  end
+
+  def select_image;end
+
+  def new
+    encouragement_request = EncouragementRequest.find(params[:request_id])
+    @encouragement_message = EncouragementMessage.new(encouragement_request_id: encouragement_request.id)
+  end
+
+  def create
+    @encouragement_message = EncouragementMessage.new(encouragement_message_params)
+    image = ImageCreator.build(@encouragement_message.text, @encouragement_message.background_id)
+    image_path = image.path
+    @encouragement_message.message_image.attach(io: File.open(image_path), filename: 'message_image.png', content_type: 'image/png')
+
+    if @encouragement_message.save
+      redirect_to encouragement_message_path(@encouragement_message), success: '画像を作成しました'
+    else
+      flash.now[:danger] = '画像の作成に失敗しました'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    @encouragement_message.update(encouragement_message_params)
+    image = ImageCreator.build(@encouragement_message.text, @encouragement_message.background_id)
+    image_path = image.path
+    @encouragement_message.message_image.attach(io: File.open(image_path), filename: 'message_image.png', content_type: 'image/png')
+    if @encouragement_message.save
+      redirect_to encouragement_message_path(@encouragement_message), success: '画像を作成しました'
+    else
+      flash.now[:danger] = '画像の作成に失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @encouragement_message.destroy
+    redirect_to root_path, success: 'メッセージカードの送信を取り消しました'
+  end
+
+  private
+
+  def set_encouragement_message
+    @encouragement_message = EncouragementMessage.find_by(id: params[:id])
+  end
+
+  def encouragement_message_params
+    params.require(:encouragement_message).permit(:text, :background_id, :encouragement_request_id)
+  end
+end
