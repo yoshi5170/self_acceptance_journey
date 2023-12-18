213d720e8f20fb952d5c3be2b19e15c940f7d66b
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-12-18 15:39:39 +0900

    add: statusカラムのバリーデーションを追加

--- app/models/encouragement_request.rb
+++ app/models/encouragement_request.rb
@@ -3,6 +3,8 @@ class EncouragementRequest < ApplicationRecord
   belongs_to :user
   has_many :encouragement_messages
   validates :text, presence: true, length: { maximum: 150 }
+  validates :status, presence: true
   validates :background_id, presence: true, numericality: { only_integer: true }
   validates :request_image, presence: true, blob: { content_type: ['image/png'] }
+  enum status: { draft: 0, public_status: 1 }
 end
