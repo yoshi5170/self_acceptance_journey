931b4f6207651b412b05bf073c4836f44d974f43
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-12-20 00:36:03 +0900

    add: 文字数と回数制限の処理を追加

--- app/controllers/self_discovery_trainings_controller.rb
+++ app/controllers/self_discovery_trainings_controller.rb
@@ -5,15 +5,15 @@ class SelfDiscoveryTrainingsController < ApplicationController
 
   def result
     # 今日のトレーニング回数を取得
-    # today_trainings_count = current_user.self_dis_trainings.where('trained_at >= ?', Time.zone.now.beginning_of_day).count
+    today_trainings_count = current_user.self_discovery_trainings.where('trained_at >= ?', Time.zone.now.beginning_of_day).count
 
-    # if text_params.length > 50
-    #   flash.now[:danger] = t('.text_limit')
-    #   render :new
-    #   return
-    # end
+    if text_params.length > 50
+      flash.now[:danger] = t('.text_limit')
+      render :new
+      return
+    end
 
-    # if today_trainings_count < 2
+    if today_trainings_count < 2
       # OpenAIへのリクエストを行い、結果を取得
       additional_prompt = '自己分析の専門家として、ユーザーの短所を60文字以内で長所に変換してください。具体例を用い、[短所]は実は[長所]です。'
       Rails.logger.info 'Preparing to send request to OpenAI...'
@@ -30,13 +30,15 @@ class SelfDiscoveryTrainingsController < ApplicationController
       Rails.logger.info "Received response from OpenAI: #{response.inspect}"
       @chat = response.dig('choices', 0, 'message', 'content') if response.present?
 
-      if @chat.blank?
+      if @chat.present?
+        current_user.self_discovery_trainings.create!(trained_at: Time.current)
+      else
         flash.now[:danger] = t('.fail')
         render :new
       end
-    # else
-    #   redirect_to root_path, danger: t('.over_the_limit')
-    # end
+    else
+      redirect_to root_path, danger: t('.over_the_limit')
+    end
   end
 
   private
