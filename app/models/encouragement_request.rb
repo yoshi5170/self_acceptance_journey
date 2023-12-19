21456324eb01dcb0775241f036576d76e43dcf9c
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-12-19 23:51:27 +0900

    add: dependent: :destroy オプションを追加

--- app/models/encouragement_request.rb
+++ app/models/encouragement_request.rb
@@ -1,7 +1,7 @@
 class EncouragementRequest < ApplicationRecord
   has_one_attached :request_image
   belongs_to :user
-  has_many :encouragement_messages
+  has_many :encouragement_messages, dependent: :destroy
   validates :text, presence: true, length: { maximum: 150 }
   validates :status, presence: true
   validates :background_id, presence: true, numericality: { only_integer: true }
